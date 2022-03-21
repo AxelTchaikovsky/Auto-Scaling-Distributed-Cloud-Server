8962ef3815b6d5ea6b052de04840de07568ffb44
李紫轩 <zli3@andrew.cmu.edu>
2022-03-21 03:04:53 -0400

    Checkpoint 1 done

--- src/Server.java
+++ src/Server.java
@@ -4,7 +4,20 @@ public class Server {
   public static void main(String args[]) throws Exception {
     if (args.length != 3) throw new Exception("Need 3 args: <cloud_ip> <cloud_port> <VM id>");
     ServerLib SL = new ServerLib(args[0], Integer.parseInt(args[1]));
+    int vmId = Integer.parseInt(args[2]);
 
+    System.err.println("[ Time: " + SL.getTime() + " ]");
+    int serverNum = 0;
+    if (SL.getTime() <= 6.0) {
+      serverNum = 2;
+    } else {
+      serverNum = 4;
+    }
+    if (vmId == 1) {
+      for (int i = 0; i < serverNum; i++) {
+        SL.startVM();
+      }
+    }
     // register with load balancer so requests are sent to this server
     SL.register_frontend();
 
